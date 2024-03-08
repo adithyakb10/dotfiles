3999d90ff489743b53ac6ad2849ab05c7964c121
Adithya K B <adithyakb93@gmail.com>
2024-03-08 12:03:28 +0530

    Initial commit

---
+++ .config/nvim/lua/autocmds.lua
@@ -0,0 +1,8 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set clipboard=unnamedplus")
+vim.cmd("set number")
+vim.cmd("set ma")
+vim.g.mapleader = " "
